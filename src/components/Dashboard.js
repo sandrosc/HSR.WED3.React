6db7a859aa3db391cb9ce66d1a11d3e0a31ba235
Sandro Scheiwiller <sandro@012345.ch>
2018-03-08 18:29:46 +0100

    handle account stuff

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 
-import { getTransactions } from '../api';
+import { getTransactions, getAccount } from '../api';
+
+import FormElement from './FormElement';
 
 export type Props = {
-  token: String
+  token: string
 };
 
 class Dashboard extends Component<Props, {}> {
+  state = {
+    from: '',
+    to: '',
+    toAccount: {
+      searched: false,
+      found: false,
+      accountNr: '',
+      owner: ''
+    },
+    amount: 0,
+    transactionError: undefined
+  };
+
   componentDidMount() {
     const { token } = this.props;
     getTransactions(token).then(
@@ -15,35 +30,84 @@ class Dashboard extends Component<Props, {}> {
     );
   }
 
-  render() {
-    const newTransactionError = false;
+  handleInputChanged = (field, value) => {
+    this.setState(state => ({ ...state, [field]: value }));
+    if (field === 'to') {
+      this.handleToUpdated(value);
+    }
+  };
 
+  handleToUpdated = value => {
+    if (!value) {
+      this.setState(state => ({
+        ...state,
+        toAccount: { ...state.toAccount, searched: false }
+      }));
+      return;
+    }
+    getAccount(value, this.props.token).then(
+      value => {
+        this.setState(state => ({
+          ...state,
+          toAccount: {
+            searched: true,
+            found: true,
+            accountNr: value.accountNr,
+            owner: `${value.owner.firstname} ${value.owner.lastname}`
+          }
+        }));
+      },
+      error => {
+        this.setState(state => ({
+          ...state,
+          toAccount: {
+            ...state.toAccount,
+            searched: true,
+            found: false
+          }
+        }));
+      }
+    );
+  };
+
+  render() {
     return (
       <div>
         <h1>Dashboard</h1>
         <div className="dashboardContents">
           <form className="newTransaction">
-            {/* <label>
-            Username
-            <input
-              onChange={this.handleLoginChanged}
-              placeholder="Login"
-              value={this.state.login}
+            <FormElement
+              label="Von"
+              field="from"
+              value={this.state.from}
+              onChange={this.handleInputChanged}
             />
-          </label>
-          <label>
-            Passwort
-            <input
-              onChange={this.handlePasswordChanged}
-              placeholder="Password"
-              type="password"
-              value={this.state.password}
-            /> 
-          </label>*/}
+            <FormElement
+              label="Zu"
+              field="to"
+              value={this.state.to}
+              onChange={this.handleInputChanged}
+            />
+            {this.state.toAccount.searched && (
+              <div className="formInfo">
+                {this.state.toAccount.found
+                  ? this.state.toAccount.owner
+                  : 'Accountnr. nicht gefunden'}
+              </div>
+            )}
+            <FormElement
+              label="Menge"
+              field="amount"
+              value={this.state.amount}
+              onChange={this.handleInputChanged}
+              type="number"
+            />
             <div>
-              <button onClick={this.handleSubmit}>Log-in</button>
+              <button onClick={this.handleSubmit}>Überweisen</button>
             </div>
-            {newTransactionError && <p>Es ist ein Fehler aufgetreten!</p>}
+            {this.state.transactionError && (
+              <p>Es ist ein Fehler aufgetreten!</p>
+            )}
           </form>
         </div>
         <div />
